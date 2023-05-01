80240bf60512afe4599eb0df79fa1ecbab8ec95b
Christian Pansch <christian.pansch@uxma.com>
2023-05-01 19:12:17 +0200

    add login again

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Client, Events, GatewayIntentBits, Collection } from 'discord.js'
 
 import { commands } from './commands'
+import { DISCORD_BOT_TOKEN } from './config'
 import { ready, interactionCreate } from './events'
 import { SlashCommand } from './types'
 
@@ -14,7 +15,9 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.DirectMessages],
 })
 client.commands = new Collection()
+
 commands.forEach((command) => client.commands.set(command.data.name, command))
 
 client.once(Events.ClientReady, ready)
 client.on(Events.InteractionCreate, interactionCreate)
+client.login(DISCORD_BOT_TOKEN)
